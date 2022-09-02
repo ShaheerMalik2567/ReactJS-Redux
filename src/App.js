48194f6f8627ddcbf19827909c53d582183561ae
sherjilmalik <sherjil.malik@ideofuzion.com>
2022-09-02 14:19:11 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+
+import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
+
+import React from "react";
+
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  return (
+    <React.Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+
+      <Counter />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../Store/Redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
